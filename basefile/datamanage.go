964d01cc8447c3d8a36746a61304621c4bb3abfb
drakepass <you@example.com>
2019-07-13 13:05:03 +0800

    add new libs

---
+++ basefile/datamanage.go
@@ -0,0 +1,31 @@
+package drakelib
+
+import (
+	"reflect"
+	"strings"
+)
+
+//判断元素是否在string array sli map中
+func IsExistIn(arr, e interface{}) bool {
+	val := reflect.ValueOf(arr)
+	switch val.Kind() {
+	case reflect.String:
+		if reflect.TypeOf(e).Kind() == reflect.String {
+			return strings.Contains(arr.(string), e.(string))
+		}
+	case reflect.Array, reflect.Slice:
+		length := val.Len()
+		for i := 0; i < length; i++ {
+			if reflect.DeepEqual(val.Index(1).Interface(), e) {
+				return true
+			}
+		}
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			if reflect.DeepEqual(val.MapIndex(key).Interface(), e) {
+				return true
+			}
+		}
+	}
+	return false
+}
--- basefile/datetime.go
+++ basefile/datetime.go
@@ -1,6 +1,8 @@
 package drakelib
 
-import "time"
+import (
+	"time"
+)
 
 const (
 	Nanosecond  time.Duration = 1
@@ -9,6 +11,7 @@ const (
 	Second                    = 1000 * Millisecond
 	Minute                    = 60 * Second
 	Hour                      = 60 * Minute
+	Day                       = 24 * Hour
 )
 
 //计时器，进程内记录多个断点执行时间
@@ -38,4 +41,29 @@ func (t *TimeCounter) TmpPoint() {
 }
 func (t *TimeCounter) GetCountTime() []time.Duration {
 	return t.TimeQueue
+}
+
+//传入北京时间(月日时分秒要采用两位表示)，获取北京时间戳
+func Strtotime(beijingtime string) (int64, error) {
+	layout := "2006-01-02 15:04:05"
+	time.Local, _ = time.LoadLocation("Asia/Chongqing")
+	t, err := time.ParseInLocation(layout, beijingtime, time.Local)
+	if err != nil {
+		return 0, err
+	}
+	return t.Unix(), nil
+}
+
+//将时间戳转换格式（2006-01-02 15.04.05）的时间字符串,可根据传入的duration精确到具体单位
+func Timeformat(timestamp int64, duration time.Duration) string {
+	layout := "2006-01-02 15:04:05"
+	switch duration {
+	case Minute:
+		layout = "2006-01-02 13:04"
+	case Hour:
+		layout = "2006-01-02 13"
+	case Day:
+		layout = "2006-01-02"
+	}
+	return time.Unix(timestamp, 0).Format(layout)
 }
